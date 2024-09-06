3e0c54e5a9c4618c9feff8144b9ffb4c038e35f3
Zeki Burak Özden <zburakozden@gmail.com>
2024-09-06 21:17:51 +0300

    Egitim bitisi ve dosyalarin kaydedilmesi

---
+++ 3 CRM Analytics/Case Study 1/FLO_benim_cozumum.py
@@ -0,0 +1,5 @@
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
